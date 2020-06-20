4031a7e85d4ebf73823a52bc458e80fc2af04cdf
Egoruch <65445916+Egoruch@users.noreply.github.com>
2020-06-20 12:10:07 +0300

    Update filter_sma.h

--- filter_sma.h
+++ filter_sma.h
@@ -1,14 +1,12 @@
 #include "main.h"
 
+/* Choose filter order */
+#define FILTER_SMA_ORDER 12
+
 /**
   * @brief Simple Moving Average (SMA) filter.
   * @note Before use define filter order.
   * @param[in] Input raw (unfiltered) value.
   * @retval Return filtered data.
   */
-
-#define Filter_SMA_Order 6 // Choose filter order
-
-uint16_t Filter_Buffer[Filter_SMA_Order] = {0,};
-
-uint16_t Filter_SMA(uint16_t To_Filtered);
+uint16_t Filter_SMA(uint16_t For_Filtered);
