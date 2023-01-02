4e70fd1d9fdf77a6b4e76ba160c16ce4dad79ffd
Yehor <65445916+Egoruch@users.noreply.github.com>
2023-01-02 22:35:29 +0200

    fix: fully update and optimize library

--- filter_sma.c
+++ filter_sma.c
@@ -1,35 +1,37 @@
 #include "filter_sma.h"
 
-uint16_t Filter_Buffer[FILTER_SMA_ORDER] = {0,};
+/* Structure BEGIN */
+sma_s SMA[SMA_QUANTITY];
+/* Structure END */
 
 /**
-  * @brief Simple Moving Average (SMA) filter.
-  * @note Before use define filter order.
-  * @param Input raw (unfiltered) value.
-  * @retval Return filtered data.
+  * @brief Simple Moving Average (SMA) filter
+  * @note Before use define filter order
+  * @param Filter name (see enumeration), Input raw (unfiltered) uint16_t value
+  * @retval Return filtered data
   */
-	uint16_t Filter_SMA(uint16_t For_Filtered)
+	uint16_t Filter_SMA(sma_name_e name, uint16_t input_value)
 {
-	/* Load new value */
-	Filter_Buffer[FILTER_SMA_ORDER - 1] = For_Filtered;
-	
-	/* For output value */
-	uint32_t Output = 0;
-	
-	/* Sum */
-	for(uint8_t i = 0; i < FILTER_SMA_ORDER; i++)
-	{
-		Output += Filter_Buffer[i];
-	}
-	
-	/* Divide */
-	Output /= FILTER_SMA_ORDER;
-	
-	/* Left Shift */
-	for(uint8_t i = 0; i < FILTER_SMA_ORDER; i++)
-				Filter_Buffer[i] = Filter_Buffer[i+1];
+	/* Subtract the oldest stored value */
+	SMA[name].Sum -= SMA[name].Buffer[SMA[name].Index];
 
-	/* Return filtered value */
-	return (uint16_t) Output;
-	
+	/* Add new value to the sum */
+	SMA[name].Sum += input_value;
+
+	/* Store current value */
+	SMA[name].Buffer[SMA[name].Index] = input_value;
+
+	/* Increment index */
+	SMA[name].Index++;
+
+	/* If index bigger is equal of bigger than filter order (window) -> nullify it */
+	if (SMA[name].Index >= FILTER_SMA_ORDER){
+		SMA[name].Index = 0;
+	}
+
+	/* Divide sum by filter order */
+	uint16_t output_value =  SMA[name].Sum/FILTER_SMA_ORDER;
+
+	/* Return filtered data */
+	return output_value;
 }
--- filter_sma.h
+++ filter_sma.h
@@ -4,14 +4,37 @@
 #include "main.h"
 
 /* Choose filter order */
-#define FILTER_SMA_ORDER 12
+
+#define FILTER_SMA_ORDER 64
+
+/* Enumerations BEGIN */
+
+typedef enum FilterName{
+	SMA_1 = 0,
+	SMA_2,
+
+	SMA_QUANTITY
+}sma_name_e;
+
+/* Enumerations END */
+
+/* Each filter should have this set of variables */
+typedef struct SMA_Params{
+	uint32_t Sum;
+	uint8_t Index;
+	uint16_t Buffer[FILTER_SMA_ORDER];
+}sma_s;
+
+/* Prototype BEGIN */
 
 /**
-  * @brief Simple Moving Average (SMA) filter.
-  * @note Before use define filter order.
-  * @param Input raw (unfiltered) value.
-  * @retval Return filtered data.
+  * @brief Simple Moving Average (SMA) filter
+  * @note Before use define filter order
+  * @param Filter name (enumeration), Input raw (unfiltered) uint16_t value
+  * @retval Return filtered data
   */
-uint16_t Filter_SMA(uint16_t For_Filtered);
+uint16_t Filter_SMA(sma_name_e num, uint16_t For_Filtered);
+
+/* Prototype END */
 
 #endif /* INC_SMA_H_ */
